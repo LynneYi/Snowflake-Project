fd764a5b24f4e9d2ae7e5010c212a1af7bf7f7cc
LynneYi <linfenglynne@gmail.com>
2024-05-16 20:31:00 +1200

    Update Total Revenue Code Snipt

--- Pizza-Analytics-Project/SQL-Scripts/Pizza-Analytics.sql
+++ Pizza-Analytics-Project/SQL-Scripts/Pizza-Analytics.sql
@@ -4,11 +4,10 @@
 -- Changelog:
  -- Draft Created on 16/05/2024 by Lynne
  -- v0.1 Created on <DD/MM/YYYY> by Lynne for <Reason>
- -- ....
+ -- v0.2 Created on <DD/MM/YYYY> by Lynne for calculating total revenue
 */
 
 ---------------Script Start Here------------------
 SELECT 
-  *
-FROM <tableA>
-where ......
+  SUM(total_price) AS Total_Revenue 
+From pizza_sales 
