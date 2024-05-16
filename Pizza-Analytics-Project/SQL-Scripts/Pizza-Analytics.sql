2894b3385cd5930e4a6ed1f665caf97dfb0b21b4
LynneYi <linfenglynne@gmail.com>
2024-05-16 20:25:03 +1200

    Create Pizza-Analytics.sql

---
+++ Pizza-Analytics-Project/SQL-Scripts/Pizza-Analytics.sql
@@ -0,0 +1,14 @@
+/*
+-- Project Name: This project is used for upskilling my Snowflake skills in terms of DDL and DML
+-- Author: Lynne Yi
+-- Changelog:
+ -- Draft Created on 16/05/2024 by Lynne
+ -- v0.1 Created on <DD/MM/YYYY> by Lynne for <Reason>
+ -- ....
+*/
+
+---------------Script Start Here------------------
+SELECT 
+  *
+FROM <tableA>
+where ......
