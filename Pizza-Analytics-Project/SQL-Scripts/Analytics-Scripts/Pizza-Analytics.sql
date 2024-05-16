d2fc8735ca4cd1c26902e35fe13ff5120810c7b5
LynneYi <linfenglynne@gmail.com>
2024-05-16 21:02:18 +1200

    Move file

---
+++ Pizza-Analytics-Project/SQL-Scripts/Analytics-Scripts/Pizza-Analytics.sql
@@ -0,0 +1,14 @@
+/*
+-- Project Name: This project is used for upskilling my Snowflake skills in terms of DDL and DML
+-- Author: Lynne Yi
+-- Changelog:
+ -- Draft Created on 16/05/2024 by Lynne
+ -- v0.1 Created on <DD/MM/YYYY> by Lynne for <Reason>
+ -- v0.2 Created on <DD/MM/YYYY> by Lynne for calculating KPI
+*/
+
+---------------Script Start Here------------------
+SELECT 
+  SUM(total_price) AS Total_Revenue 
+From PIZZASUMMARY.PIZZASUMMARY.pizza_sales 
+;
